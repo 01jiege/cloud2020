4a1719773e6bd620a2da47cf0ad4f84ce7f9200e
yangjie <yhfin2016>
2022-03-24 16:18:54 +0800

    更新

--- cloud-consumer-order80/src/main/java/com/atguigu/springcloud/controller/ConsumerController.java
+++ cloud-consumer-order80/src/main/java/com/atguigu/springcloud/controller/ConsumerController.java
@@ -1,8 +1,10 @@
 package com.atguigu.springcloud.controller;
 
 import com.atguigu.springcloud.entities.CommonResult;
+import com.atguigu.springcloud.entities.Payment;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
 
@@ -12,14 +14,28 @@ import javax.annotation.Resource;
 @Slf4j
 public class ConsumerController {
 
+    private static final String PAYMENT_URL = "http://localhost:8001";
+
     @Resource
     private RestTemplate restTemplate;
 
-    @GetMapping(value ="/get/{id}")
-    public CommonResult getByID(int id){
+    @GetMapping(value ="/consumer/payment/get/{id}")
+    public CommonResult getPaymentById(@PathVariable("id") Long id){
         log.info("消费查询");
-        return restTemplate.getForObject("http://localhost:8001/payment/get/1",CommonResult.class);
+        return restTemplate.getForObject(PAYMENT_URL+"/payment/get/"+id,CommonResult.class);
     }
+
+    @GetMapping(value = "/consumer/payment/create")
+    public CommonResult create(Payment payment){
+        log.info("添加对象");
+        return restTemplate.postForObject(PAYMENT_URL+"/payment/create",payment,CommonResult.class);
+    }
+
+    @GetMapping(value = "/test1")
+    public CommonResult test1(){
+        return new CommonResult(11,"34");
+    }
+
 
 
 
